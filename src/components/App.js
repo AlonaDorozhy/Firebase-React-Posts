48189aa4739e223e56a26d2b3623cbcccf645622
Alona <dorozhynskaad@gmail.com>
2019-01-14 17:40:02 +0200

    blog3

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,24 @@
 import React, { Component } from 'react';
 import { database } from '../firebase';
 import _ from 'lodash';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import renderHTML from 'react-render-html';
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       title: '',
       body: '',
-      post: ''
-    }
-    this.onInputChange = this.onInputChange.bind(this);
+      posts: {}
+    };
+    // bind
+    this.onHandleChange = this.onHandleChange.bind(this);
     this.onHandleSubmit = this.onHandleSubmit.bind(this);
   }
 
-  onInputChange(e) {
-    this.setState({
-      [e.target.name]: e.target.value
-    })
-  }
-
-  onHandleSubmit(e) {
-    e.preventDefault();
-    const post = {
-      title: this.state.title,
-      body: this.state.body
-    }
-
-    database.push(post);
-    this.setState({
-      title: '',
-      body: '',
-      post: ''
-    })
-  }
-
+  // lifecycle
   componentDidMount() {
     database.on('value', snapshot => {
       this.setState({
@@ -42,64 +26,99 @@ class App extends Component {
       });
     });
   }
+
+  // render posts from firebase
   renderPosts() {
     return _.map(this.state.posts, (post, key) => {
       return (
         <div key={key}>
           <h2>{post.title}</h2>
-          <p>{post.body}</p>
+          <p>{renderHTML(post.body)}</p>
         </div>
       );
     });
   }
+
+  onHandleChange(e) {
+    this.setState({ body: e });
+    console.log(this.state.body);
+  }
+
+  onHandleSubmit(e) {
+    e.preventDefault();
+    const post = {
+      title: this.state.title,
+      body: this.state.body
+    };
+    database.push(post);
+    this.setState({
+      title: '',
+      body: ''
+    });
+  }
+
   render() {
     return (
       <div className="container">
-        <form className='subForm' onSubmit={this.onHandleSubmit}>
+        <form onSubmit={this.onHandleSubmit}>
           <div className="form-group">
             <input
               value={this.state.title}
               type="text"
               name="title"
               placeholder="Title"
-              onChange={this.onInputChange}
-              // onChange={e => {
-              //   this.setState({ title: e.target.value });
-              // }}
+              onChange={e => {
+                this.setState({ title: e.target.value });
+              }}
               ref="title"
-              className="form-control"
-            />
-            <input
-              value={this.state.body}
-              type="text"
-              name="body"
-              placeholder="Body"
-              onChange={this.onInputChange}
-              // onChange={e => {
-              //   this.setState({ title: e.target.value });
-              // }}
-              ref="body"
               className="form-control"
             />
           </div>
           <div className="form-group">
-            {this.state.body}
-            {/* <ReactQuill
+            <ReactQuill
               modules={App.modules}
               formats={App.formats}
               value={this.state.body}
               placeholder="Body"
               onChange={this.onHandleChange}
-            /> */}
+            />
           </div>
           <button className="btn btn-primary">Post</button>
         </form>
         <br />
-        <div>
-        {this.renderPosts()}</div>
+        {this.renderPosts()}
       </div>
     );
   }
 }
+
+App.modules = {
+  toolbar: [
+    [{ header: '1' }, { header: '2' }, { font: [] }],
+    [{ size: [] }],
+    ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+    [{ list: 'ordered' }, { list: 'bullet' }],
+    ['link', 'image', 'video'],
+    ['clean'],
+    ['code-block']
+  ]
+};
+
+App.formats = [
+  'header',
+  'font',
+  'size',
+  'bold',
+  'italic',
+  'underline',
+  'strike',
+  'blockquote',
+  'list',
+  'bullet',
+  'link',
+  'image',
+  'video',
+  'code-block'
+];
 
 export default App;
